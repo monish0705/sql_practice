58541101274105c34dc3e0959ec434f244890e90
Monish mr <monishmr2006@gmail.com>
2026-09-17 11:22:37 +0530

    completed window aggregate function

--- windows_function_basics/windows_function.sql
+++ windows_function_basics/windows_function.sql
@@ -46,3 +46,80 @@ group by ProductID
  Sales,
  sum(Sales) over(partition by OrderStatus Order by OrderDate) totalsales
  from Sales.Orders
+
+ --PART TO WHOLE COMPARISION
+
+ --find the percentage contribution of each product sales to total sales.
+
+ select 
+ OrderID,
+ ProductID,
+ Sales,
+ sum(Sales) over() totalsales,
+ round(cast (Sales  as float)/  sum(Sales) over() *100 ,2) perofeachsales
+ from Sales.Orders
+
+
+
+ --find the avg score of customers
+ select 
+ CustomerID,
+ LastName,
+ Score,
+ AVG(Coalesce (Score,0)) over() avgscore
+ from Sales.Customers
+
+
+ --find all orders where sales are higher  than the average sales acrosss all orders.
+
+ select *
+ from(
+	 select
+	 OrderID,
+	 ProductID,
+	 Sales,
+	 avg(sales) over() avgsales
+
+	 from Sales.Orders
+	 )t where Sales > avgsales    -- window function cannot be used in where clause 
+
+
+
+
+--find the employes who have highest salaries.
+
+
+select *
+from(
+	select *,
+	max(Salary) over() highestsalary
+	from Sales.Employees
+	)t where Salary = highestsalary
+
+
+
+--find the deviation of each sales from the minimum and maximum sales 
+
+select 
+OrderID,
+OrderDate,
+ProductID,
+Sales,
+Max(Sales) over () maxsales,
+Min(Sales) over() minsales,
+Sales - Min(Sales) over() deviationOverMin,
+max(Sales) over() - Sales  deviationOverMax
+from Sales.Orders
+
+--calculate moving average  of sales for each product over the time , including the next order.
+
+
+
+select 
+OrderID,
+OrderDate,
+ProductID,
+Sales,
+avg(Sales) over(partition by ProductID order by OrderDate) avgSales,
+avg(Sales) over(partition by ProductID order by OrderDate rows between current row and 1 following ) avgSalesOverTime
+from Sales.Orders
